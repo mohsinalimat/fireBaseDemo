6f37c3394943f47d7915fb9992344da7382e3898
Vincent Smithers <vsmithers1087@gmail.com>
2016-10-14 18:54:33 -0700

    storyboard skeleton

---
+++ SocialtopiasDemo/SocialtopiasDemo/CreateProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateProfileViewController.h
+//  SocialtopiasDemo
+//
+//  Created by Vincent Smithers on 10/14/16.
+//  Copyright © 2016 Vince Smithers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateProfileViewController : UIViewController
+
+@end
