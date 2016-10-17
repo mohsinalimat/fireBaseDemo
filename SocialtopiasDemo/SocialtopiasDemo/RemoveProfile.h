48d96983ea6f71f7b8ea356b2d3fc357ea83702b
Vincent Smithers <vsmithers1087@gmail.com>
2016-10-17 16:18:07 -0700

    begin ViewController refactor

---
+++ SocialtopiasDemo/SocialtopiasDemo/RemoveProfile.h
@@ -0,0 +1,13 @@
+//
+//  RemoveProfile.h
+//  SocialtopiasDemo
+//
+//  Created by Vincent Smithers on 10/17/16.
+//  Copyright © 2016 Vince Smithers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RemoveProfile : NSObject
+
+@end
---
+++ SocialtopiasDemo/SocialtopiasDemo/RemoveProfile.m
@@ -0,0 +1,13 @@
+//
+//  RemoveProfile.m
+//  SocialtopiasDemo
+//
+//  Created by Vincent Smithers on 10/17/16.
+//  Copyright © 2016 Vince Smithers. All rights reserved.
+//
+
+#import "RemoveProfile.h"
+
+@implementation RemoveProfile
+
+@end
